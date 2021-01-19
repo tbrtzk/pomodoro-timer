ea0faa83bc042f20d3b6a3abaf7ff8d90b351607
Tabea Ratzke <tabratzke@gmail.com>
2021-01-19 07:34:37 +0100

    Add README.md

---
+++ README.md
@@ -0,0 +1,8 @@
+# Pomodoro Timer App
+The Pomodoro Timer is a solution for time management based on the pomodoro technique.
+
+
+## Introduction
+While learning Python I created this app to study the use of the tkinter GUI package.
+
+![App Screenshot](pomodoro_timer.png)
--- main.py
+++ main.py
@@ -30,9 +30,9 @@ def reset_timer():
 def start_timer():
     global reps
     reps += 1
-    work_sec = WORK_MIN
-    short_break_sec = SHORT_BREAK_MIN
-    long_break_sec = LONG_BREAK_MIN
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
 
     if reps % 8 == 0:
         count_down(long_break_sec)
